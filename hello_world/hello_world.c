579189b9352e2f8369be2e9c782e86e4c5b0891c
Ivan Lin <ivan7645@gmail.com>
2018-07-23 22:43:26 +0800

    Modify Makefile to ldd3's format.

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -1,11 +1,13 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/sched.h>
+
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
 	printk(KERN_ALERT "Hello, world\n");
+
 	printk(KERN_INFO "The current precess is \"%s\" (pid %i)\n",
 	       current->comm, current->pid);
 
